fdc5b63031300ae85b05242c4dd461d412eff130
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-03-10 21:45:56 +0530

    Update Main.java

--- 7. Oops/1. Encapsulation/0. Encapsulation Basics/Main.java
+++ 7. Oops/1. Encapsulation/0. Encapsulation Basics/Main.java
@@ -5,7 +5,12 @@ public class Main {
         Student s1 = new Student();
         //s1.name = "Kali";  //not possible because name entitiy is private
         s1.setName("Kali Linux");       //setName change the value after calling the setName function
+        s1.setAge(20);
+        s1.setGender("male");
+
         System.out.print(s1.getName());  //getName return the value
+        System.out.println(s1.getAge());
+        System.out.println(s1.getGender());
 
     }
 }
@@ -16,27 +21,23 @@ class Student {
     private int age;
     private String gender;
 
-    //setter
+    //setter Function
     public void setName(String name) {          //non static kyunki access krni hai main function me ess studnet class ke function ko;
         this.name = name;
     }
-
-    public String getName() {
-        return name;
-    }
-
     public void setAge(int age) {
         this.age = age;
     }
-
-    public int getAge() {
-        return age;
-    }
-
     public void setGender(String gender) {
         this.gender = gender;
     }
-
+    // Getter Function
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
     public String getGender() {
         return gender;
     }
